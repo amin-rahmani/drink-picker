5ed435baeb8aa0a04e38e804cfa5ad2468b11d17
amin <94103247+amin-rahmani@users.noreply.github.com>
2022-12-09 13:59:50 +0330

    we can upload from api now

---
+++ script.js
@@ -0,0 +1,27 @@
+const drinkContainer = document.querySelector('#drink');
+
+document.querySelector('#search-by-name').addEventListener('submit', (event)=> {
+    event.preventDefault();
+    // console.log(event);
+    // console.log(event.target);
+    // console.log(event.target.elements);
+    const query = event.target.elements[0].value.trim();
+    // console.log({query});
+    // console.log({euc: encodeURIComponent(query),enc: encodeURI(query)});
+    const params = new URLSearchParams({s:query})
+    console.log(params.toString());
+    const url = new URL('https://www.thecocktaildb.com/api/json/v1/1/search.php?');
+    url.searchParams.set('s',query)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            displayDrink(data.drinks[0])
+        });
+});
+
+function displayDrink(drink){
+    drinkContainer.querySelector('h2').textContent = drink.strDrink;
+    drinkContainer.querySelector('img').src = drink.strDrinkThumb;
+    drinkContainer.querySelector('p').textContent = drink.strInstructions
+}
