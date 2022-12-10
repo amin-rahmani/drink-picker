7cfbebfd8fc659a713e76fe33ba83520a63b1d34
amin <94103247+amin-rahmani@users.noreply.github.com>
2022-12-10 15:38:31 +0330

    changing alot

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const drinkContainer = document.querySelector('#drink');
 
-document.querySelector('#search-by-name').addEventListener('submit', (event)=> {
+document.querySelector('#search-by-name').addEventListener('submit', (event) => {
     event.preventDefault();
     // console.log(event);
     // console.log(event.target);
@@ -8,10 +8,10 @@ document.querySelector('#search-by-name').addEventListener('submit', (event)=> {
     const query = event.target.elements[0].value.trim();
     // console.log({query});
     // console.log({euc: encodeURIComponent(query),enc: encodeURI(query)});
-    const params = new URLSearchParams({s:query})
+    const params = new URLSearchParams({ s: query })
     console.log(params.toString());
     const url = new URL('https://www.thecocktaildb.com/api/json/v1/1/search.php?');
-    url.searchParams.set('s',query)
+    url.searchParams.set('s', query)
     fetch(url)
         .then(res => res.json())
         .then(data => {
@@ -20,10 +20,30 @@ document.querySelector('#search-by-name').addEventListener('submit', (event)=> {
         });
 });
 
-function displayDrink(drink){
+function displayDrink(drink) {
     window.d = drink;
     drinkContainer.querySelector('h2').textContent = drink.strDrink;
     drinkContainer.querySelector('img').src = drink.strDrinkThumb;
-    drinkContainer.querySelector('p').innerHTML = drink.strInstructions
+    drinkContainer.querySelector('p').innerHTML = drink.strInstructions;
+
+
+    let html = '';
+    for (let i = 1; i <= 15; i++) {
+        const ingredient = drink[`strIngredient${i}`];
+        if (!ingredient) break;
+        const measure = drink[`strMeasure${i}`] || '';
+        // console.log(ingredient,measure);
+        html += `<tr><td><img src="https://www.thecocktaildb.com/images/ingredients/${ingredient}-Small.png" alt="${ingredient}" /></td><td>${measure}</td></tr>`
+    };
+
+    drinkContainer.querySelector('#ingredients-table').innerHTML = html;
+    for (const[left,key] of Object.entries({
+        Alcoholic: 'strAlcoholic',
+        Category: 'strCategory',
+        Glass:'strGlass',
+        Modified:'dateModified'
+    })){
+
+    }
 }
 // displayDrink()
